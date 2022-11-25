98684edb3207127ae31f69baaef1abd3fd4b25fb
Yash Bhalani <1870639+marvadia@users.noreply.github.com>
2022-11-25 17:15:54 -0400

    fix: path-not found for new defined API routes.
    Problem : The RunApp.java file was defined in com.ems.controller package which limited the new API routes to be defined in that package only.
    
    Solution:
    Move the RunApp.java file in com.ems package which makes it accessible for the application to reach all the routes defined in the sub-packages.

--- src/main/java/com/ems/RunApp.java
+++ src/main/java/com/ems/RunApp.java
@@ -1,4 +1,4 @@
-package com.ems.controller;
+package com.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RunApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RunApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RunApp.class, args);
+    }
 
 }
