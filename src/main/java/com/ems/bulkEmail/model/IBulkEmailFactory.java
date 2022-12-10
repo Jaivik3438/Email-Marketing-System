1ae850049ed96237765a89bc86d95fa615595655
lpatel <lv842182@dal.ca>
2022-12-10 14:24:21 -0400

    commit

--- src/main/java/com/ems/bulkEmail/model/IBulkEmailFactory.java
+++ src/main/java/com/ems/bulkEmail/model/IBulkEmailFactory.java
@@ -1,7 +1,8 @@
 package com.ems.bulkEmail.model;
 
 import com.ems.campaign.model.Campaign;
+import com.ems.subscriberList.model.SubscriberList;
 
 public interface IBulkEmailFactory {
-    public BulkEmail CreateBulkEmail(Campaign campaign);
+    public BulkEmail CreateBulkEmail(Campaign campaign, SubscriberList subscriberList);
 }
