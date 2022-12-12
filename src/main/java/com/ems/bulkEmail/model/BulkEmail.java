f3d6933d08d6b3dd00ea7a8096694459b44641d4
lpatel <lv842182@dal.ca>
2022-12-11 21:31:17 -0400

    build fix

--- src/main/java/com/ems/bulkEmail/model/BulkEmail.java
+++ src/main/java/com/ems/bulkEmail/model/BulkEmail.java
@@ -7,13 +7,13 @@ import com.ems.campaign.model.IObserver;
 import com.ems.campaign.model.Subject;
 import com.ems.subscriberList.model.Subscriber;
 import com.ems.subscriberList.model.SubscriberList;
+import com.ems.subscriberList.persistence.SubscriberDB;
 
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
+
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
+
 import java.util.List;
 
 public class BulkEmail implements IObserver {
@@ -31,7 +31,6 @@ public class BulkEmail implements IObserver {
 
     @Override
     public void update(Subject s) {
-<<<<<<< HEAD
         emailDetailsList=generateEmailDetailList();
         for(int i=0;i<emailDetailsList.size();i++){
            EmailDetails emailDetail=emailDetailsList.get(i);
@@ -45,13 +44,17 @@ public class BulkEmail implements IObserver {
         }
     }
     private List<EmailDetails> generateEmailDetailList(){
-        subscribers=subscriberList.getSubscriberByCampaignID(campaign.getCampaignId());
+        try {
+            subscribers=subscriberList.getSubscriberByCampaignID(campaign.getCampaignId(),new SubscriberDB(MySqlPersistenceConnection.getInstance().getConnection()));
+        } catch (Exception e) {
+             e.printStackTrace();
+        }
         List<EmailDetails> emailList=new ArrayList<>();
         EmailDetailBuilder emailDetailBuilder= new SimpleEmailDetailBuilder();
         for (Subscriber subscriber:subscribers){
             EmailDetails emailDetails=emailDetailBuilder.buildEmailDetail(subscriber);
             emailDetails.campaignId=campaign.getCampaignId();
-            Mail mail=emailDetailBuilder.buildEmail(campaign.getTemplate(),new HtmlFormatter());
+            Mail mail=emailDetailBuilder.buildEmail(campaign.getEmailTemplate(),new HtmlFormatter());
             emailDetails.mail=mail;
             emailList.add(emailDetails);
         }
@@ -60,8 +63,5 @@ public class BulkEmail implements IObserver {
     private void sendEmail(EmailDetails emailDetail){
         ISendEmail sendEmail= new Gmail("emsprojectasdc@gmail.com","jtuagavmuwwqzkxt",emailDetail);
         sendEmail.sendEmail(emailDetail);
-=======
-//        subscribers=subscriberList.getSubscriberByCampaignID(campaign.getCampaignId());
->>>>>>> origin/develop
     }
 }
