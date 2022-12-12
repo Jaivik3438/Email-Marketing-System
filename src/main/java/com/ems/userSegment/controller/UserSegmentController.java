2b4d20fd40a739e67a3cc616acf17f63cf6574fd
Yash Bhalani <ys602824@dal.ca>
2022-12-12 16:11:08 -0400

    refactor: create seperate module and configure code for user-segment

---
+++ src/main/java/com/ems/userSegment/controller/UserSegmentController.java
@@ -0,0 +1,40 @@
+package com.ems.userSegment.controller;
+
+import com.ems.DbConnection.MySqlPersistenceConnection;
+import com.ems.authentication.model.User;
+import com.ems.userSegment.model.UserSegment;
+import com.ems.userSegment.persistence.IUserSegmentPersistent;
+import com.ems.userSegment.persistence.UserSegmentDB;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
+
+@Controller()
+@RequestMapping("/api")
+public class UserSegmentController {
+
+    @GetMapping("/user-segment")
+    public UserSegment getUserSegmentByUserId(HttpServletRequest request, HttpSession session){
+        User user = (User) session.getAttribute("user");
+
+        String userId = user.userId;
+
+        try{
+            IUserSegmentPersistent userSegmentPersistent = new UserSegmentDB(MySqlPersistenceConnection.getInstance().getConnection());
+            UserSegment userSegment = userSegmentPersistent.getUserSegmentByUserId(userId);
+            if(userSegment != null){
+                return userSegment;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
+}
