f7189fe39bf8cf88d1fee03607367288e6257109
snpatel <sh732170@dal.ca>
2022-12-14 13:10:30 -0400

    Added email_template modules tests

--- src/main/java/com/ems/email_template/model/ITemplateFactory.java
+++ src/main/java/com/ems/email_template/model/ITemplateFactory.java
@@ -3,4 +3,5 @@ package com.ems.email_template.model;
 public interface ITemplateFactory {
     EmailTemplate createSimpleEmailTemplate(String templateName);
     EmailTemplate createSimpleEmailTemplate();
+    EmailTemplate createSimpleEmailTemplate(String id, String templateName, String templateSubject, String templateDescription, String landingPageLink);
 }
